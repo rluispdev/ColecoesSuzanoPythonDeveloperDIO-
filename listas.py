6a63968ce6879a0c6681ae87c025187d0bf83489
rluispdev <rluis@icloud.com>
2025-02-28 16:02:53 -0300

    Listas

---
+++ listas.py
@@ -0,0 +1,14 @@
+frutas = [ "laranja", "maçã", "uva", "pera", "mamão" ]
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list("Python")
+print(letras)
+
+numeros = list(range(1, 11))
+print(numeros)
+
+carro = ["Ferrari", "F8", 34000, 2020, 2900, "São Paulo", True ]
+print(carro)
