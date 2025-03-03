b2cd2452b0115153a4dad394276a24daec3bde61
rluispdev <rluis@icloud.com>
2025-03-03 14:04:31 -0300

    Listas

--- listas.py
+++ listas.py
@@ -12,3 +12,15 @@ print(numeros)
 
 carro = ["Ferrari", "F8", 34000, 2020, 2900, "São Paulo", True ]
 print(carro)
+
+
+
+numeros = [ 1, 30, 21, 2, 9, 65, 34]
+
+pares = []
+
+for numero in numeros:
+    if numero % 2 == 0:
+        pares.append(numero)
+
+print(pares)
